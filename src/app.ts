0609377136fd7c921056b14484f51c6114f3cebe
Marco Rodrigues <marcoaurelio853@gmail.com>
2022-12-11 19:49:29 +0000

    teste

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import './../app.css'
 
 
+//autobind decorator
+
+function autobind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this)
+      return boundFn
+    }
+
+  };
+  return adjDescriptor;
+}
+
+// Project Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -27,9 +43,49 @@ class ProjectInput {
     this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
     this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
 
+    this.configure()
     this.attach();
   }
 
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (enteredTitle.trim().length === 0
+      || enteredDescription.trim().length === 0
+      || enteredPeople.trim().length === 0) {
+      alert("Please enter a valid input")
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople]
+    }
+  }
+
+
+  private clearInput() {
+
+    this.peopleInputElement.value = ''
+    this.titleInputElement.value = ''
+    this.descriptionInputElement.value = ''
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput()
+
+    // validate if has a tuple once tuple is array
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput
+      this.clearInput()
+    }
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler)
+  }
 
   /**
    *
